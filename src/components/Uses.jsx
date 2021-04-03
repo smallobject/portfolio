33532393d1e93608d4d83a8529fa09850dced489
smallobject <tiesti.os@gmail.com>
2021-04-03 23:57:51 +0300

    testing how links will look on the main site and added links to items

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -50,6 +50,23 @@ const Uses = ({ showUses, setModalView }) => {
                 alt=''
                 className='office-img m-auto rounded-md shadow-sm mt-2 mb-2'
               />
+              <div>
+                <ul className='text-gray-200 text-sm m-2'>
+                  <li>
+                    <a
+                      rel='noreferrer'
+                      target='_blank'
+                      className='m-blue'
+                      href='https://www.amazon.com/LG-32GK650F-B-Monitor-FreeSync-Technology/dp/B07FLGR2PN/ref=sr_1_4?dchild=1&keywords=LG+UltraGear+32GK650F-B&qid=1617483269&sr=8-4'
+                    >
+                      {' '}
+                      32" LG UltraGear 32GK650F-B Monitor (Main)
+                    </a>
+                  </li>
+                  <li> 27" AOC CQ27G2U/BK (Vertical)</li>
+                  <li> Something else</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
