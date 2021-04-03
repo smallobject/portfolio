bd811fbeca92a6171b5e412260479ccb4b4d94e4
smallobject <tiesti.os@gmail.com>
2021-04-03 23:43:04 +0300

    added a home office image and style fixes to the modal

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import HomeOffice from '../assets/images/homeoffice.jpg';
 
 import './uses.css';
 
 const Uses = ({ showUses, setModalView }) => {
+  //creating a reference for our container so we do not close it
+  //if someone clicks inside of it
+  let containerRef = useRef();
   //Adding effect to close the modal from click anywhere outside of it
   //Removing the eventlistener after the ffect has happened
   useEffect(() => {
     if (showUses) {
-      let clickOutsideEvent = () => {
-        setModalView();
+      let clickOutsideEvent = (e) => {
+        if (!containerRef.current.contains(e.target)) {
+          setModalView();
+        }
       };
       document.addEventListener('mousedown', clickOutsideEvent);
 
@@ -22,22 +28,29 @@ const Uses = ({ showUses, setModalView }) => {
     return (
       <div className='overlay-bg'>
         <div className='overlay-container'>
-          <div className='uses-container'>
+          <div className='uses-container' ref={containerRef}>
             <span
               className='cursor-pointer font-bold float-right m-2 text-xs text-red-500'
               onClick={() => setModalView()}
             >
               CLOSE ME
             </span>
-            <h1 className='text-gray-200 jetbrains text-3xl text-center mt-4'>
-              What I use
+            <h1 className='text-gray-200 font-bold text-3xl text-center mt-4 jetbrains m-blue'>
+              WHAT I USE
             </h1>
-            <button className='uses-button' onClick={() => setModalView()}>
+            {/* <button className='uses-button' onClick={() => setModalView()}>
               Close Modal
-            </button>
-            <dir>
-              <h1>Computer Setup</h1>
-            </dir>
+            </button> */}
+            <div>
+              <h1 className='text-gray-200 jetbrains bg-black w-max p-1 m-2'>
+                {'//Home Office Setup'}
+              </h1>
+              <img
+                src={HomeOffice}
+                alt=''
+                className='office-img m-auto rounded-md shadow-sm mt-2 mb-2'
+              />
+            </div>
           </div>
         </div>
       </div>
